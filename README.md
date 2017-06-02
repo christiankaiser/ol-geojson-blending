fc971a4a7eda5f5208fba98302006b026aa21d89
Christian Kaiser <ck@naxio.ch>
2017-06-02 13:00:01 +0200

    Example map OL GeoJSON multiply

---
+++ README.md
@@ -0,0 +1,7 @@
+# Example map with OpenLayers and GeoJSON
+
+This example map shows how to create a GeoJSON overlay with OpenLayers, using 'multiply' as blending mode and normal blending mode for stroke and labels.
+
+Here is an image how the map looks like:
+
+![](img/preview-map.jpg)
---
+++ js/index.js
@@ -0,0 +1,78 @@
+var baseLayer = new ol.layer.Tile({
+  source: new ol.source.XYZ({
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+  }),
+  opacity: 1
+});
+
+var col = function(reg){
+  var c = {
+    BONV: '#ff9c5c',
+    ORBE: '#8ba2ff',
+    COTM: '#b0ff45',
+    CHAB: '#ff735c',
+    COTN: '#2effc1',
+    VULL: '#2eff41',
+    LAVU: '#d773ff',
+    LAVR: '#ff73ae',
+  }[reg];
+  return (c || '#fff');
+}
+var regViticFillStyle = function(feature, resolution){
+  return new ol.style.Style({
+    fill: new ol.style.Fill({ color: col(feature.getProperties().regstat) }),
+  })
+};
+var regViticStrokeStyle = function(feature, resolution){
+  fontsize = resolution > 200 ? '12px' : '14px'
+  return new ol.style.Style({
+    stroke: new ol.style.Stroke({ color: '#fff', width: 1 }),
+    text: new ol.style.Text({
+      textAlign: 'center',
+      font: fontsize+'/1.2 Verdana, sans-serif',
+      text: resolution > 400 ? '' : feature.getProperties().nom.replace(' (', "\n("),
+      fill: new ol.style.Fill({color: '#000'}),
+      stroke: new ol.style.Stroke({ color: 'rgba(255, 255, 255, 0.5)', width: 2 })
+    })
+  })
+};
+
+var overlayFill = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'data/reg-vitic-stats-wgs84.geojson',
+    format: new ol.format.GeoJSON()
+  }),
+  style: regViticFillStyle
+});
+
+var overlayStroke = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'data/reg-vitic-stats-wgs84.geojson',
+    format: new ol.format.GeoJSON()
+  }),
+  style: regViticStrokeStyle
+});
+
+var blendModeMultiply = function(evt) {
+  evt.context.globalCompositeOperation = 'multiply';
+};
+var blendModeNormal = function(evt) {
+  evt.context.globalCompositeOperation = 'normal';
+};
+overlayFill.on('precompose', blendModeMultiply);
+overlayStroke.on('precompose', blendModeNormal)
+
+var map = new ol.Map({
+  target: 'map',
+  layers: [baseLayer, overlayFill, overlayStroke],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([6.703, 46.6]),
+    zoom: 9,
+    extent: [682500, 5810300, 801200, 5939700],
+    maxZoom: 14,
+    minZoom: 8
+  })
+});
+
+map.getView().fit([682500, 5810300, 801200, 5939700]);
+
